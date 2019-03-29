44c13bbc74f7884e62a0dfb2c0a54c304db036c8
pchauhan-qlogic <paras.chauhan@qlogic.io>
2019-03-29 21:23:06 +0530

    [updated] - merging two repo automatically

--- merge_two_repo.py
+++ merge_two_repo.py
@@ -1,10 +1,7 @@
-import os
-import re, time
-
-import requests
+import os, time, requests
 from synthtool import shell
 
-OWNER = "pmakani"
+OWNER = "paraschauhance"
 GITHUB = "https://github.com"
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -13,9 +10,8 @@ is_cloned = os.path.isdir(os.path.join(BASE_DIR, ".git"))
 class GitAutoMergeFork(object):
     def __init__(self, user_name):
         self.username = user_name
-        self.repo = "google-cloud-java1"
-        # self.private_repo = self.repo + "-private"
-        self.private_repo = "google-cloud-java" + "-private"
+        self.repo = "google-cloud-java"
+        self.private_repo = self.repo + "-private"
 
         self.repo1 = "{}/{}/{}".format(GITHUB, OWNER, self.repo)
         self.fork1 = "{}/{}/{}".format(GITHUB, self.username, self.repo)
@@ -30,9 +26,6 @@ class GitAutoMergeFork(object):
     def add_n_fetch_remote_repo(self):
         if is_cloned is False:
             shell.run(["git", "init"])
-            # shell.run(["git", "clone", self.fork1])
-        # if not self.check_repo_exist("origin"):
-        #     shell.run(["git", "clone", self.fork1])
         repo_list = (
             (self.repo1_remote_name, self.repo1),
             (self.repo2_remote_name, self.repo2),
@@ -42,20 +35,10 @@ class GitAutoMergeFork(object):
         for repo_name, url in repo_list:
             if self.is_repo_exist_on_git(url):
                 self.add_remote_url(repo_name, url)
-                shell.run(["git", "fetch", repo_name])
             else:
                 print("{} url have not repo exist".format(url))
                 sys.exit()
 
-        # self.add_remote_url(self.repo1_remote_name, self.repo1)
-        # self.add_remote_url(self.repo2_remote_name, self.repo2)
-        # self.add_remote_url(self.fork1_remote_name, self.fork1)
-        # self.add_remote_url(self.fork2_remote_name, self.fork2)
-        # shell.run(["git", "fetch", self.repo1_remote_name])
-        # shell.run(["git", "fetch", self.repo2_remote_name])
-        # shell.run(["git", "fetch", self.fork1_remote_name])
-        # shell.run(["git", "fetch", self.fork2_remote_name])
-        # shell.run(["git", "fetch", "origin"])
 
     def is_repo_exist_on_git(self, url):
         is_exist = False
@@ -80,21 +63,11 @@ class GitAutoMergeFork(object):
             return remote_name in output.stdout
         return False
 
-    def has_more_diff(self, from_repo, to_repo):
+    def has_diff(self, from_repo, to_repo):
         has_more_changes = False
-        # ["git",  "diff", "master", "against_fork/master" ,"--shortstat"]
-        # output = shell.run(["git", "diff",  "fork1/master", "fork2/master", "--shortstat"])
-        # output =  208 files changed, 39625 insertions(+), 9481 deletions(-)
         output = shell.run(["git", "diff",  "{}/master".format(from_repo), "{}/master".format(to_repo), "--shortstat"])
         if output.stdout:
-            print(output.stdout)
-            insertion_str = re.findall("\d+ insertion", output.stdout)
-            if insertion_str:
-                count_list = re.findall("\d+", insertion_str[0])
-                if count_list:
-                    count = int(count_list[0])
-                    if count > 0:
-                        has_more_changes = True
+            has_more_changes = True
         return has_more_changes
 
 if __name__ == '__main__':
@@ -106,31 +79,56 @@ if __name__ == '__main__':
     print(username)
     git_merge = GitAutoMergeFork(username)
     git_merge.add_n_fetch_remote_repo()
+    shell.run(["git", "fetch", "--all"])
+    is_diff = git_merge.has_diff(git_merge.repo1_remote_name, git_merge.repo2_remote_name)
+    if is_diff:
+        is_conflict = False
+        branch_name = "comparing_branch_{}".format(int(time.time()))
+        shell.run(["git", "checkout", "{}/master".format(git_merge.repo1_remote_name), "-B", branch_name])
+        try:
+            merge_code = shell.run(["git", "merge", "{}/master".format(git_merge.repo2_remote_name), "--no-commit"])
+        except Exception as e:
+            is_conflict = True
+            shell.run(["git", "reset", "--hard"])
 
-    if git_merge.has_more_diff(git_merge.fork1_remote_name, git_merge.fork2_remote_name):
-        print("more difference beetween {} master from {} master".format(git_merge.fork1_remote_name,
-                                                                         git_merge.fork2_remote_name))
-        branch_name = "sync_branch_{}".format(int(time.time()))
-        shell.run(["git", "checkout", "{}/master".format(git_merge.fork2_remote_name), "-f"])
-        shell.run(["git", "checkout", "-b", branch_name])
-        shell.run(["git", "push", git_merge.fork1_remote_name, branch_name, "-f"])
 
-        print("{} branch has been pushed successfully.".format(branch_name))
-        out = shell.run(["hub", "pull-request", "-b", "{}/{}:master".format(OWNER, git_merge.repo),
-                         "-h", "{}/{}:{}".format(git_merge.username, git_merge.repo, branch_name), "-m",
-                         "msg for sync branch {} to master".format(branch_name)])
-        print(out.stdout)
+        if is_conflict:
+            branch_name = "comparing_branch_{}".format(int(time.time()))
+            shell.run(["git", "checkout", "{}/master".format(git_merge.repo1_remote_name), "-B", branch_name])
+            shell.run(["git", "push", git_merge.fork2_remote_name, branch_name, "-f"])
+            print("{} branch has been pushed successfully.".format(branch_name))
+            out = shell.run(["hub", "pull-request", "-b", "{}/{}:master".format(OWNER, git_merge.private_repo),
+                             "-h", "{}/{}:{}".format(git_merge.username, git_merge.private_repo, branch_name), "-m",
+                             "msg for sync branch {} to master".format(branch_name)])
+            print(out.stdout)
 
-    if git_merge.has_more_diff(git_merge.fork2_remote_name, git_merge.fork1_remote_name):
-        print("more difference beetween {} master from {} master".format(git_merge.fork2_remote_name,
-                                                                         git_merge.fork1_remote_name))
-        branch_name = "sync_branch_{}".format(int(time.time()))
-        shell.run(["git", "checkout", "{}/master".format(git_merge.fork1_remote_name), "-f"])
-        shell.run(["git", "checkout", "-b", branch_name])
-        shell.run(["git", "push", git_merge.fork2_remote_name, branch_name, "-f"])
+            branch_name = "comparing_branch_{}".format(int(time.time()))
+            shell.run(["git", "checkout", "{}/master".format(git_merge.repo2_remote_name), "-B", branch_name])
+            shell.run(["git", "push", git_merge.fork1_remote_name, branch_name, "-f"])
+            print("{} branch has been pushed successfully.".format(branch_name))
+            out = shell.run(["hub", "pull-request", "-b", "{}/{}:master".format(OWNER, git_merge.repo),
+                             "-h", "{}/{}:{}".format(git_merge.username, git_merge.repo, branch_name), "-m",
+                             "msg for sync branch {} to master".format(branch_name)])
+            print(out.stdout)
+        else:
+            merge_br_repo1_diff = shell.run(["git", "diff", "{}/master".format(git_merge.repo1_remote_name),
+                                             branch_name, "--shortstat"])
+            merge_br_repo2_diff = shell.run(["git", "diff", "{}/master".format(git_merge.repo2_remote_name),
+                                             branch_name, "--shortstat"])
 
-        print("{} branch has been pushed successfully.".format(branch_name))
-        out = shell.run(["hub", "pull-request", "-b", "{}/{}:master".format(OWNER, git_merge.repo + "-private"),
-                   "-h",  "{}/{}:{}".format(git_merge.username, git_merge.repo + "-private", branch_name), "-m",
-                   "msg for sync branch {} to master".format(branch_name)])
-        print(out.stdout)
+            if merge_br_repo1_diff.stdout is "":
+                shell.run(["git", "push", git_merge.fork2_remote_name, branch_name, "-f"])
+                print("{} branch has been pushed successfully.".format(branch_name))
+                out = shell.run(["hub", "pull-request", "-b", "{}/{}:master".format(OWNER, git_merge.private_repo),
+                                 "-h", "{}/{}:{}".format(git_merge.username, git_merge.private_repo, branch_name), "-m",
+                                 "msg for sync branch {} to master".format(branch_name)])
+                print(out.stdout)
+
+            if merge_br_repo2_diff.stdout is "":
+
+                shell.run(["git", "push", git_merge.fork1_remote_name, branch_name, "-f"])
+                print("{} branch has been pushed successfully.".format(branch_name))
+                out = shell.run(["hub", "pull-request", "-b", "{}/{}:master".format(OWNER, git_merge.repo),
+                                 "-h", "{}/{}:{}".format(git_merge.username, git_merge.repo, branch_name), "-m",
+                                 "msg for sync branch {} to master".format(branch_name)])
+                print(out.stdout)
